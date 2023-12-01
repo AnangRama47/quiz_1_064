68910c414a0fc43b102a22b0d85e39360d285744
AnangRama47 <145915367+AnangRama47@users.noreply.github.com>
2023-12-01 16:12:58 +0800

    Add files via upload

---
+++ Quiz KB/Quiz Kecerdasan Buatan.py
@@ -0,0 +1,60 @@
+class MatrixOperations:
+    def _init_(self, matrix):
+        self.matrix = matrix
+
+    def find_min_max(self):
+        min_value = min(min(row) for row in self.matrix)
+        max_value = max(max(row) for row in self.matrix)
+        return min_value, max_value
+
+    def transpose(self):
+        transposed_matrix = [[self.matrix[j][i] for j in range(len(self.matrix))] for i in range(len(self.matrix[0]))]
+        return transposed_matrix
+
+    def matrix_multiplication(self, other_matrix):
+        if len(self.matrix[0]) != len(other_matrix):
+            raise ValueError("The number of columns in the first matrix must be equal to the number of rows in the second matrix.")
+        
+        result_matrix = [[sum(a * b for a, b in zip(row, col)) for col in zip(*other_matrix)] for row in self.matrix]
+        return result_matrix
+
+    def matrix_addition(self, other_matrix):
+        if len(self.matrix) != len(other_matrix) or len(self.matrix[0]) != len(other_matrix[0]):
+            raise ValueError("The matrices must have the same dimensions for addition.")
+        
+        result_matrix = [[self.matrix[i][j] + other_matrix[i][j] for j in range(len(self.matrix[0]))] for i in range(len(self.matrix))]
+        return result_matrix
+
+
+if __name__ == "_main_":
+    matrix_a = [
+        [34, 100, 12],
+        [72, 24, 55],
+        [61, 20, 19]
+    ]
+
+    matrix_operations = MatrixOperations(matrix_a)
+
+    # Menghitung element terbesar dan terkecil
+    min_value, max_value = matrix_operations.find_min_max()
+    print(f"Minimum value: {min_value}")
+    print(f"Maximum value: {max_value}")
+
+    # Transpose matrix
+    transposed_matrix = matrix_operations.transpose()
+    print("Transposed matrix:")
+    for row in transposed_matrix:
+        print(row)
+
+    # Menghitung perkalian matrix (A) dan (T)
+    matrix_b = matrix_operations.transpose()
+    multiplication_result = matrix_operations.matrix_multiplication(matrix_b)
+    print("Matrix multiplication result:")
+    for row in multiplication_result:
+        print(row)
+
+    # Menghitung penjumlahan matrix (T) dan (A)
+    addition_result = matrix_operations.matrix_addition(matrix_b)
+    print("Matrix addition result:")
+    for row in addition_result:
+        print(row)
